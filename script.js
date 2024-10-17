760aebe66d182d6e0f35ac7980f4d35fe2a9c6bd
Aresxl <lockyyw@outlook.com>
2024-10-18 10:12:43 +1100

    Updated HTML to fix some issues with the form submit and clear button work now

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+const bookTitleInput = document.getElementById("bookTitle");
+const authorNameInput = document.getElementById("authorName");
+const bookPagesInput = document.getElementById("bookPages");
+const readYesInput = document.getElementById("readYes");
+const readNoInput = document.getElementById("readNo");
+
+const cancelBtn = document.getElementById("cancelBtn");
+const submitBtn = document.getElementById("submitBtn");
+
+
+cancelBtn.addEventListener('click', clearForm);
+submitBtn.addEventListener('click', submitForm);
+
+function submitForm(event) {
+    event.preventDefault();
+
+    const bookTitle = bookTitleInput.value;
+    const authorName = authorNameInput.value;
+    const bookPages = bookPagesInput.value;
+    const readStatus = readYesInput.checked ? 'Yes' : 'No'
+
+    console.log("Title: ", bookTitle);
+    console.log("Author: ", authorName);
+    console.log("Pages: ", bookPages);
+    console.log("Read Status: ", readStatus);
+
+    clearForm();
+}
+
+function clearForm() {
+
+    bookTitleInput.value = '';
+    authorNameInput.value = ''
+    bookPagesInput.value = ''
+    readYesInput.checked = false;
+    readNoInput.checked = false;
+}
+
+
+
+
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
